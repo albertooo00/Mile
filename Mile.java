4df97d563b667f85af3fbf9901c3cb90379f7d31
Albertooo Safin <sultanofsicness@icloud.com>
2021-03-15 17:55:58 +0300

    mile

---
+++ Mile.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean Mile = true;
+        long price = 100;
+        int percent = 5;
+
+        long bonus = price /  percent;
+        System.out.println(bonus);
+    }
+}
